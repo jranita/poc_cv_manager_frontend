7b0c03e2086816bfef3cdd31ba48ccad4a238ed8
jranita <14070264+jranita@users.noreply.github.com>
2024-01-09 15:16:58 +0100

    Trying to add Flowbite components, does not compile.

--- app/src/views/home.rs
+++ app/src/views/home.rs
@@ -1,4 +1,4 @@
-use dioxus::prelude::*;
+use dioxus::{prelude::*};
 pub(crate) use dioxus_router::components::Link;
 
 use crate::router::Route;
@@ -8,22 +8,25 @@ pub fn Home(cx: Scope) -> Element {
     let mut count = use_state(cx, || 0);
 
     cx.render(
-        rsx! (
-            body {
-                Link {
-                    class: "text-red-400 bg-gray-300 flex items-center justify-center",
-                    to: Route::Blog {
-                        id: *count.get()
-                    },
-                    "Go to blog!!"
-                },
-                div {
-                    h1 { "High-Five counter: {count}" }
-                    button { onclick: move |_| count += 1, "Up high!" }
-                    button { onclick: move |_| count -= 1, "Down low!" }
-
-                }
-            }
+        Html (
+            <header>
+                <script src: "../../node_modules/flowbite/dist/flowbite.min.js" />
+            </header>
+            <body>
+                // Link {
+                //     class: "text-red-400 bg-gray-300 flex items-center justify-center",
+                //     to: Route::Blog {
+                //         id: *count.get()
+                //     },
+                //     "Go to blog!!"
+                // },
+                // div {
+                //     h1 { "High-Five counter: {count}" }
+                //     button { onclick: move |_| count += 1, "Up high!" }
+                //     button { onclick: move |_| count -= 1, "Down low!" }
+                // }
+                <div inline-datepicker data-date="02/25/2022"></div>
+            </body>
         )
     )
 }
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "all",
-  content: ["./src/**/*.{rs,html,css}", "./dist/**/*.html"],
+  content: ["./src/**/*.{rs,html,css}", "./dist/**/*.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };

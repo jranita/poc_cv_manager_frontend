8d73642da189321540bb6d36605b5d20b702619d
jranita <14070264+jranita@users.noreply.github.com>
2024-01-17 16:15:29 +0100

    cors: working.

--- app/src/services/keywords.rs
+++ app/src/services/keywords.rs
@@ -1,7 +1,7 @@
 use crate::models::keyword::Keyword;
 use reqwest;
+use reqwest::header::{HeaderMap, ACCEPT, ACCESS_CONTROL_ALLOW_ORIGIN, ORIGIN};
 use reqwest::Error;
-use reqwest::header::{HeaderMap, ACCESS_CONTROL_ALLOW_ORIGIN};
 
 pub async fn get_keywords(
     limit: usize,
@@ -10,23 +10,27 @@ pub async fn get_keywords(
     order_direction: String,
     filter: String,
 ) -> Result<&'static str, Error> {
-// ) -> Result<Vec<Keyword>, reqwest::Error> {
+    // ) -> Result<Vec<Keyword>, reqwest::Error> {
 
     // let url: String = format!("{}keywords?offset={}&limit={}&order_by={}&order_direction={}&filter={}", super::BASE_API_URL, offset, limit, order_by, order_direction, filter);
     // let url: String = format!("{}keywords?offset={}&limit={}", super::BASE_API_URL, offset, limit);
     let url: String = format!("{}keywords", super::BASE_API_URL);
     let mut headers = HeaderMap::new();
-    headers.insert(ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:8080".parse().unwrap());
+    headers.insert(
+        ACCESS_CONTROL_ALLOW_ORIGIN,
+        "http://localhost:8080".parse().unwrap(),
+    );
+    headers.insert(ORIGIN, "http://localhost:8080".parse().unwrap());
+    headers.insert(ACCEPT, "application/json".parse().unwrap());
 
-    let client = reqwest::Client::builder()
-        .build()?;
+    let client = reqwest::Client::builder().build()?;
 
-    let rows = client.get(url)
+    let rows = client
+        .get(url)
         .headers(headers)
         .basic_auth("admin@admin.org", Some("password"))
-        .send().await?;
-
-    println!("{:?}", rows);
+        .send()
+        .await?;
 
     // let keywords_list = rows
     //     .iter()
--- app/src/services/mod.rs
+++ app/src/services/mod.rs
@@ -1,3 +1,5 @@
 pub mod keywords;
 
+// pub static BASE_API_URL: &str = "https://cors-anywhere.herokuapp.com/http://localhost:5800/api/";
 pub static BASE_API_URL: &str = "http://localhost:5800/api/";
+// pub static BASE_API_URL: &str = "http://192.168.88.245:5800/api/";
